de3c1e1745a6ad13ec2134f3592b1790d4ab1ad7
Yashini13 <yashupar81@gmail.com>
2025-06-20 11:05:30 +0530

    Adding changes

--- Backend/index.js
+++ Backend/index.js
@@ -144,7 +144,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -165,7 +164,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
